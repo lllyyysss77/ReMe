a2099202a0495f416c0225078cc19d51256aa926
caozouying.czy <caozouying.czy@alibaba-inc.com>
2026-01-23 17:03:54 +0800

    add: workflow/procedural_memory

---
+++ reme/workflow/procedural_memory/summarizer/__init__.py
@@ -0,0 +1,12 @@
+from ....core import R
+from .trajectory_preprocess import TrajectoryPreprocess
+from .success_extraction import SuccessExtraction
+
+__all__ = [
+    "TrajectoryPreprocess",
+    "SuccessExtraction",
+]
+
+for name in __all__:
+    tool_class = globals()[name]
+    R.op.register()(tool_class)
